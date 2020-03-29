aadcc5251afaee6ffaf901946495224a61dc7533
Sapir Nassimi <sapir.nassimi@gmail.com>
2020-03-30 01:14:01 +0300

    feat: unite two functions to one

--- src/utils/statistics.js
+++ src/utils/statistics.js
@@ -1,7 +1,13 @@
 const unirest = require('unirest');
 
-const getAllCountriesData = (callback) => {
+const getStatistics = (countryName, callback) => {
     const request = unirest('GET', 'https://covid-193.p.rapidapi.com/statistics');
+
+    if (countryName) {
+        request.query({
+            country: countryName
+        });
+    }
 
     request.headers({
         'x-rapidapi-host': 'covid-193.p.rapidapi.com',
@@ -15,34 +21,10 @@ const getAllCountriesData = (callback) => {
 
         callback(undefined, res.body);
     
-        console.log('Done fetching data');
-    });
-}
-
-const getCountryData = (countryName, callback) => {
-    const request = unirest('GET', 'https://covid-193.p.rapidapi.com/statistics');
-
-    request.query({
-        country: countryName
-    });
-
-    request.headers({
-        'x-rapidapi-host': 'covid-193.p.rapidapi.com',
-        'x-rapidapi-key': 'd7382f77damsh1447cc50e9c4f49p1b3e7cjsn48d3d0e9d300'
-    });
-
-    request.end(res => {
-        if (res.error) {
-            callback(res.error);
-        }
-    
-        callback(undefined, res.body);
-
         console.log('Done fetching data');
     });
 }
 
 module.exports = {
-    getAllCountriesData,
-    getCountryData
+    getStatistics
 }
